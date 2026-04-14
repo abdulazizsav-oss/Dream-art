94c394dbbb36f8561df6c70efced11e0569b4ad6
admin <lola@MacBook-Pro-lola.local>
2026-04-15 00:37:50 +0500

    auto: update layout.tsx

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import { Sidebar } from '@/components/layout/Sidebar'
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 min-w-0 p-6 lg:p-8">
+        {children}
+      </main>
+    </div>
+  )
+}
