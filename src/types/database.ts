aa3664b8504f395100c8b25bbb75c7ecf4bcdc0d
admin <lola@MacBook-Pro-lola.local>
2026-04-18 03:21:29 +0500

    auto: update database.ts

--- src/types/database.ts
+++ src/types/database.ts
@@ -155,6 +155,13 @@ export type Database = {
             referencedRelation: 'equipment_categories'
             referencedColumns: ['id']
           },
+          {
+            foreignKeyName: 'equipment_brand_id_fkey'
+            columns: ['brand_id']
+            isOneToOne: false
+            referencedRelation: 'brands'
+            referencedColumns: ['id']
+          },
         ]
       }
       equipment_maintenance: {
