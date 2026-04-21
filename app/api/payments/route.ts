671b20a0f0d7b4d9edfd43de446f276e876ae34e
admin <lola@MacBook-Pro-lola.local>
2026-04-21 16:44:43 +0500

    auto: update route.ts

--- app/api/payments/route.ts
+++ app/api/payments/route.ts
@@ -73,7 +73,7 @@ export async function POST(req: NextRequest) {
 
   revalidatePath('/finance')
   revalidatePath('/orders')
-  revalidatePath(`/orders/${parsed.data.order_id}`)
+  revalidatePath(`/orders/${order_id}`)
   revalidatePath('/dashboard')
 
   return NextResponse.json(data, { status: 201 })
